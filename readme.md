92b4f2054208572bc57cfc732fe5672861271ddf
Jordan Sussman <jsussman@workwithopal.com>
2021-12-12 10:31:51 -0800

    first commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# READ ME
+**Live Audio** is a live stream audio feed web player. My local revision is connected to a live feed of a local jazz station.
+
+Preview:
+![](screenshot.png)
+
+Built using:
+```
+- HTML
+- CSS
+- JavaScript
+```
+
+Before you begin:
+```
+- Update audio URL value in index.html to your preferred live audio feed
+- Update title and header (optional)
+```
---
+++ script.js
@@ -0,0 +1,32 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const audio = document.getElementById('audio');
+
+// PLAY FEED
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+};
+
+// PAUSE FEED
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+    audio.pause();
+};
+
+// LISTENER
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
